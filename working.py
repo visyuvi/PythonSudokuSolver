95b50d32a78a139483e41cc7971edba7d688143b
Your Name <you@example.com>
2023-07-05 23:27:09 +0530

    Function  to check if a number entered at a particular pos is valid or not

--- working.py
+++ working.py
@@ -11,6 +11,30 @@ board = [
 ]
 
 
+def valid(bo, num, pos):
+    # check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    # loop through all the nine elements of that box
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
